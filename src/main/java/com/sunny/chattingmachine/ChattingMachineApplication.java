22e4a860077393d6e19fabfd633a2ebfd6567e92
sunny <hsymyth@gmail.com>
2022-02-12 14:47:31 +0900

    Feat : Chatting machine first commit. Sunny's first personal project. Good Luck.

---
+++ src/main/java/com/sunny/chattingmachine/ChattingMachineApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.chattingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingMachineApplication.class, args);
+    }
+
+}
